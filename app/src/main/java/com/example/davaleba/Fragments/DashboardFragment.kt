394221cb00b7d5e680e55836985b8d5ef62a7463
Nika <nikolz.kvernadze.1@btu.edu.ge>
2020-12-09 21:03:18 +0400

    first commit

---
+++ app/src/main/java/com/example/davaleba/Fragments/DashboardFragment.kt
@@ -0,0 +1,7 @@
+package com.example.davaleba.Fragments
+
+import androidx.fragment.app.Fragment
+import com.example.davaleba.R
+
+class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
+}
